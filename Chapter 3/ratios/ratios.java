44f82c75ee97527e25ab4380b9834e77bfba938f
matterdark <johan.d.s.vonk@gmail.com>
2018-11-15 23:35:39 +0000

    final ratios

--- Chapter 3/ratios/ratios.java
+++ Chapter 3/ratios/ratios.java
@@ -33,7 +33,7 @@ public class ratios {
         // create input BufferedReader from file
         BufferedReader br = new BufferedReader(new FileReader("ratios.in"));
         
-        int[] feeds = new int[4][3];
+        int[][] feeds = new int[4][3];
 
         for (int i = 0; i < 4; i++) {
             StringTokenizer l = new StringTokenizer(br.readLine());
@@ -43,11 +43,36 @@ public class ratios {
         br.close();
 
 
-        
-
-
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ratios.out")));
-        out.println(digit);
+        boolean works = false;
+        loop: for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                for (int k = 0; k < 100; k++) {
+                    double first = 1.0*(i*feeds[1][0]+j*feeds[2][0]+k*feeds[3][0])/feeds[0][0];
+                    double second = 1.0*(i*feeds[1][1]+j*feeds[2][1]+k*feeds[3][1])/feeds[0][1];
+                    double third = 1.0*(i*feeds[1][2]+j*feeds[2][2]+k*feeds[3][2])/feeds[0][2];
+                    if (i==0&&j==38&&k==7) {
+                        System.out.println("first: "+first);
+                        System.out.println("second: "+second);
+                        System.out.println("third: "+third);
+                    }
+                    if (first!=0) {
+                        if ((first==second || feeds[0][0] == 0 || feeds[0][1] == 0) && (second==third || feeds[0][1] == 0 || feeds[0][2] == 0)) {
+                            if (first==(int)(first)) {
+                                out.println(i+" "+j+" "+k+" "+((int)first));
+                                works = true;
+                                break loop;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!works) {
+            out.println("NONE");
+        }
+
         out.close();
         // print final time taken
         System.out.println(System.currentTimeMillis() - startTime);
