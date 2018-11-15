0f58c3f33bdd31b768f6aab7b8084374ffd63a6a
matterdark <johan.d.s.vonk@gmail.com>
2018-11-15 16:44:11 +0000

    kimbits

--- Chapter 3/kimbits/kimbits.java
+++ Chapter 3/kimbits/kimbits.java
@@ -1,54 +1,63 @@
+
 /*
 ID: 100021881
 LANG: JAVA
 PROG: kimbits
 */
 
-// if you want to turn all the debug on, just find and replace "// DEBUG " with nothing
+import java.io.*;
+import java.util.*;
 
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.TreeMap;
-import java.util.HashMap;
-import java.util.Deque;
-import java.util.LinkedList;
-
-/*
- * kimbits
- */
 public class kimbits {
     public static void main(String[] args) throws Exception {
-        // set startTime to measure how long the program takes
         long startTime = System.currentTimeMillis();
-
-        // create input BufferedReader from file
         BufferedReader br = new BufferedReader(new FileReader("kimbits.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("kimbits.out")));
 
         StringTokenizer l = new StringTokenizer(br.readLine());
-        
+
         int N = Integer.parseInt(l.nextToken());
         int L = Integer.parseInt(l.nextToken());
         int I = Integer.parseInt(l.nextToken());
-
+        boolean[] res = solve(N, L, I, new boolean[N+1]);
+        String str = "";
+        for (int i = 0; i <= N; i++) {
+            str+=res[i]?"1":"0";
+        }
+        out.println(str);
         br.close();
-
-
-
-        
-
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("kimbits.out")));
-        out.println(digit);
         out.close();
-        // print final time taken
         System.out.println(System.currentTimeMillis() - startTime);
+    }
+    public static boolean[] solve (int N, int L, int I, boolean[] arr) {
+        if (I==-1) {
+            return arr;
+        }
+        int k = -1;
+        int total = 0;
+        for (k = 1; k <= N; k++) {
+            total=0;
+            int[] sums = new int[N+1];
+            sums[0]=1;
+            sums[k]=1;
+            for (int i = 1; 2*i <= k; i++) {
+                sums[i]+=sums[i-1]*(k-i+1)/i;
+                sums[k-i]=sums[i];
+            }
+            for (int i = 0; i <= L; i++) {
+                //System.out.println(k + "\t" + i + "\t" + sums[i]);
+                total+=sums[i];
+            }
+            if (total>=I) {
+                break;
+            }
+        }
+        System.out.println(N+"\t"+L+"\t"+I);
+        if (k<=N || true) {
+            arr[k]=true;
+            return solve(k-1, L-1, I-total+1, arr);
+        } else {
+            return solve(N-1, L, I-total+1, arr);
+        }
     }
 }
