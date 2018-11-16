745e2091ac9cb78f034f8226d5056612bf623593
matterdark <johan.d.s.vonk@gmail.com>
2018-11-16 23:00:11 +0000

    initial loops

--- Chapter 3/spin/spin.1.java
+++ Chapter 3/spin/spin.1.java
@@ -34,33 +34,23 @@ public class spin {
         BufferedReader br = new BufferedReader(new FileReader("spin.in"));
 
         Wheel[] wheels = new Wheel[5];
-
         for (int i = 0; i < 5; i++) {    
             StringTokenizer l = new StringTokenizer(br.readLine());
             int speed = Integer.parseInt(l.nextToken());
             int num = Integer.parseInt(l.nextToken());
-            wheels[i] = new Wheel(speed, num);
-            for (int j = 0; j < num*2; j++) {
-                wheels.cuts[i]=Integer.parseInt(l.nextToken());
+            Wedge[] wedges = new Wedge[num];
+            for (int j = 0; j < num; j++) {
+                wedges[num]=new Wedge(Integer.parseInt(l.nextToken()), Integer.parseInt(l.nextToken()));
             }
+            wheels[i] = new Wheel(speed, wedges);
         }
 
         br.close();
 
 
 
-        for (int i = 0; i < 5; i++)
-            for (int j = 0; j < wheels[i].cuts.size(); j+=2)
-                for (int k = 0; k <= wheels[i].cuts.get(j+1); k++)
-                    wheels[i].empty[(wheels[i].cuts.get(j))%360] = true;
+        
 
-        int res = -1;
-        loop: for (int t = 0; t < 360; t++) {
-            for (int i = 0; i < 5; i++){
-                int pos = (wheels[i].speed*t)%360;
-                
-            }
-        }
 
 
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("spin.out")));
@@ -70,16 +60,21 @@ public class spin {
         System.out.println(System.currentTimeMillis() - startTime);
     }
 
+
+    static class Wedge {
+        int angle;
+        int extent;
+        public Wedge(int ang, int ext) {
+            angle = ang;
+            extent = ext;
+        }
+    }
     static class Wheel {
         int speed;
-        int[] cuts;
-        boolean[] empty;
-        int[] e;
-        public Wheel(int spd, int num) {
+        Wedge[] wedges;
+        public Wheel(int spd, Wedge[] ws) {
             speed = spd;
-            cuts = new int[num*2];
-            empty = new boolean[360];
-            e = new int[360];
+            wedges = ws;
         }
     }
 }
