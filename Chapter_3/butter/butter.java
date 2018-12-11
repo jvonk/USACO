2cb242c09ec32b7e3066cb586596ce8198996885
matterdark <johan.d.s.vonk@gmail.com>
2018-12-11 00:43:03 +0000

    buttery work | very slippery

--- Chapter_3/butter/butter.java
+++ Chapter_3/butter/butter.java
@@ -12,6 +12,8 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -21,12 +23,18 @@ import java.util.TreeMap;
 import java.util.HashMap;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 /*
  * butter
  * same as comehome basically, use dijkstra's algorithm
  */
 public class butter {
+    static int N, P, C;
+    static List[] pastures;
+    static List<Integer> cows;
+    static int[][] distances;
+
     public static void main(String[] args) throws Exception {
         // set startTime to measure how long the program takes
         long startTime = System.currentTimeMillis();
@@ -35,124 +43,95 @@ public class butter {
         BufferedReader br = new BufferedReader(new FileReader("butter.in"));
 
         StringTokenizer l = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(l.nextToken());
-        int P = Integer.parseInt(l.nextToken());
-        int C = Integer.parseInt(l.nextToken());
-
-        boolean[] hasCow = new boolean[P];
-        int[] numCow = new int[P];
+        N = Integer.parseInt(l.nextToken());
+        P = Integer.parseInt(l.nextToken());
+        C = Integer.parseInt(l.nextToken());
+        cows = new ArrayList<Integer>();
+        pastures = new List[P];
+        for (int i = 0; i < P; i++) {
+            pastures[i]=new ArrayList<int[]>();
+        }
 
         for (int i = 0; i < N; i++) {
-            numCow[Integer.parseInt(br.readLine())-1]++;
+            cows.add(Integer.parseInt(br.readLine())-1);
         }
 
-        Node[] pastures = new Node[P];
-
-        for (int i = 0; i < P; i++) {
-            pastures[i] = new Node();
-        }
+        System.out.println(System.currentTimeMillis()-startTime);
 
         for (int i = 0; i < C; i++) {
             l = new StringTokenizer(br.readLine());
             int first = Integer.parseInt(l.nextToken())-1;
             int second = Integer.parseInt(l.nextToken())-1;
             int distance = Integer.parseInt(l.nextToken());
-            pastures[first].edges.add(new Edge(second, distance));
-            pastures[second].edges.add(new Edge(first, distance));
+            pastures[first].add(new int[] {second, distance});
+            pastures[second].add(new int[] {first, distance});
         }
+        distances=new int[P][P];
+        System.out.println(System.currentTimeMillis()-startTime);
 
         br.close();
 
-        System.out.println(System.currentTimeMillis() - startTime);
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < P; i++) {
-            Node[] calculated = dijkstra(pastures, pastures[i]);
-            int total = 0;
-            for (int j = 0; j < P; j++) {
-                total+=calculated[j].distance*numCow[j];
-            }
-            if (total<min) {
-                min = total;
-            }
-        }
-
 
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("butter.out")));
-        out.println(min);
+        out.println(solve());
         out.close();
         // print final time taken
         System.out.println(System.currentTimeMillis() - startTime);
+        System.exit(0);
     }
-
-    public static Node[] dijkstra(Node[] nodes, Node source) {
-        // distance(j) is distance from source vertex to vertex j
-        // parent(j) is the vertex that precedes vertex j in any shortest path (to
-        // reconstruct the path subsequently)
-
-        for (int i = 0; i < nodes.length; i++) {
-            nodes[i].set();
-        }
-        source.distance = 0;
-        source.parent = -1;
-
-        int numVisited = 0;
-        while (numVisited < nodes.length) {
-            // find unvisited vertex with min distance to source; call it vertex i
-            int mini = 0;
-            int min = Integer.MAX_VALUE;
-            for (int i = 0; i < nodes.length; i++) {
-                if (!nodes[i].visited && nodes[i].distance < min) {
-                    mini = i;
-                    min = nodes[i].distance;
-                }
-            }
-            nodes[mini].visited = true;
-
-            for (int i = 0; i < nodes[mini].edges.size(); i++) {
-                Edge edge = nodes[mini].edges.get(i);
-                if (nodes[mini].distance + edge.weight < nodes[edge.destination].distance) {
-                    nodes[edge.destination].distance = nodes[mini].distance + edge.weight;
-                    nodes[edge.destination].parent = i;
-                }
-            }
-            numVisited++;
-        }
-        return nodes;
-    }
-
-    public static class Edge {
-        int destination;
-        int weight;
-
-        public Edge(int destination, int weight) {
-            this.destination = destination;
-            this.weight = weight;
-        }
-    }
-
-    public static class Node {
+    public static class Node implements Comparable {
         int distance;
         boolean visited;
-        int parent;
-        List<Edge> edges = new ArrayList<>();
-
-        public Node(int distance, boolean visited, int parent) {
-            this.distance = distance;
-            this.visited = visited;
-            this.parent = parent;
+        int id;
+        @Override
+        public int compareTo(Object other) {
+            return this.distance-((Node)other).distance;
         }
-
-        public Node() {
-            this.distance = Integer.MAX_VALUE;
-            this.visited = false;
-            this.parent = -1;
+    }
+    public static int solve() {
+        int min = Integer.MIN_VALUE;
+        for (int cow : cows) {
+            dijkstra(cow);
         }
-
-        public void set() {
-            this.distance = Integer.MAX_VALUE;
-            this.visited = false;
-            this.parent = -1;
+        for (int i = 0; i < P; i++) {
+            int result = 0;
+            for (int cow : cows) {
+                result += distances[cow][i];
+                if (result == Integer.MAX_VALUE) break;
+            }
+            min=Math.min(result, min);
+        }
+        return min;
+    }
+    public static void dijkstra(int begin) {
+        Node[] nodes = new Node[P];
+        for (int i = 0; i < P; i++) {
+            nodes[i]=new Node();
+            nodes[i].id = i;
+            nodes[i].distance=Integer.MAX_VALUE;
+            nodes[i].visited=false;            
+        }
+        PriorityQueue<Node> heap = new PriorityQueue<Node>();
+        nodes[begin].distance=0;
+        nodes[begin].visited=true;
+        Collections.addAll(heap, nodes);
+        while(!heap.isEmpty()) {
+            Node node = heap.poll();
+            if (node.distance==Integer.MAX_VALUE) break;
+            node.visited=true;
+            for (Object object : pastures[node.id]) {
+                int[] arr = (int[])object;
+                if (nodes[arr[0]].distance>node.distance+arr[1]) {
+                    nodes[arr[0]].distance=node.distance+arr[1];
+                    if (!nodes[arr[0]].visited) {
+                        heap.remove(nodes[arr[0]]);
+                        heap.add(nodes[arr[0]]);
+                    }
+                }
+            }
+        }
+        for (Node node: nodes) {
+            distances[begin][node.id]=node.distance;
         }
     }
 }
