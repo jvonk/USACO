758bf4a5ed665f9c2296a6101dc01eb3baf290aa
Johan Vonk <johan.d.s.vonk@gmail.com>
2019-01-12 06:13:53 -0800

    first working camelot

--- Chapter_3/camelot/camelot.java
+++ Chapter_3/camelot/camelot.java
@@ -26,8 +26,8 @@ public class camelot {
         int R = firstLine[0];
         int C = firstLine[1];
         int numKnights = 0;
-        int[] changeRow = new int[] {-2, -1, 1, 2, 2, 1, -1, -2};
-        int[] changeCol = new int[] {1, 2, 2, 1, -1, -2, -2, -1};
+        int[] changeRow = new int[] { -2, -1, 1, 2, 2, 1, -1, -2 };
+        int[] changeCol = new int[] { 1, 2, 2, 1, -1, -2, -2, -1 };
         int[][][][] mem = new int[32][32][32][32];
         for (int i = 0; i < mem.length; i++) {
             for (int j = 0; j < mem[i].length; j++) {
@@ -37,16 +37,16 @@ public class camelot {
             }
         }
         String[] str = br.readLine().split(" ");
-        Player king = new Player(str[0].charAt(0)-'A'+1, str[1].charAt(0)-'1'+1);
+        Player king = new Player(str[0].charAt(0) - 'A' + 1, str[1].charAt(0) - '1' + 1);
         Player[] knights = new Player[1001];
         for (int i = 0; i < knights.length; i++) {
             knights[i] = new Player();
         }
         String line;
-        while ((line=br.readLine())!= null) {
+        while ((line = br.readLine()) != null) {
             str = line.split(" ");
-            for (int i = 0; i < str.length; i+=2) {
-                knights[++numKnights] = new Player(str[0].charAt(0)-'A'+1, str[1].charAt(0)-'1'+1);
+            for (int i = 0; i < str.length; i += 2) {
+                knights[++numKnights] = new Player(str[0].charAt(0) - 'A' + 1, str[1].charAt(0) - '1' + 1);
             }
         }
         br.close();
@@ -54,14 +54,15 @@ public class camelot {
             for (int j = 1; j <= C; j++) {
                 Queue<Player> queue = new LinkedList<Player>();
                 queue.add(new Player(i, j));
-                mem[i][j][i][j]=0;
-                while(!queue.isEmpty()) {
+                mem[i][j][i][j] = 0;
+                while (!queue.isEmpty()) {
                     Player player = queue.poll();
                     for (int stepType = 0; stepType < 8; stepType++) {
                         int newRow = player.row + changeRow[stepType];
                         int newCol = player.col + changeCol[stepType];
-                        if (newRow >= 0 && newCol >=0 && newRow <= R && newCol <=C && mem[i][j][newRow][newCol]==0x01010101) {
-                            mem[i][j][newRow][newCol] = mem[i][j][player.row][player.col]+1;
+                        if (newRow >= 0 && newCol >= 0 && newRow <= R && newCol <= C
+                                && mem[i][j][newRow][newCol] == 0x01010101) {
+                            mem[i][j][newRow][newCol] = mem[i][j][player.row][player.col] + 1;
                             queue.add(new Player(newRow, newCol));
                         }
                     }
@@ -69,25 +70,29 @@ public class camelot {
             }
         }
         int l = 2;
-        int kingStartRow = Math.max(1, king.row-l);
-        int kingStartCol = Math.max(1, king.col-l);
-        int kingEndRow = Math.min(R, king.row+l);
-        int kingEndCol = Math.min(C, king.col+l);
-        int minstep = 1<<25;
+        int kingStartRow = Math.max(1, king.row - l);
+        int kingStartCol = Math.max(1, king.col - l);
+        int kingEndRow = Math.min(R, king.row + l);
+        int kingEndCol = Math.min(C, king.col + l);
+        int minstep = 1 << 25;
         for (int i = 1; i <= R; i++) {
             for (int j = 1; j <= C; j++) {
                 int step = 0;
-                for(int k = 1; k <= numKnights; k++) {
-                    step+=mem[knights[k].row][knights[k].col][i][j];
+                for (int k = 1; k <= numKnights; k++) {
+                    step += mem[knights[k].row][knights[k].col][i][j];
                 }
-                int min = Math.max(Math.abs(king.row-i), Math.abs(king.col-j));
-                for (int pi = kingStartRow; pi <= kingEndRow; pi++)
-                for (int pj = kingStartCol; pj <= kingEndCol; pj++)
-                for(int k = 1; k <= numKnights; k++) {
-                    int temp = Math.max(Math.abs(king.row-pi), Math.abs(king.col-pj))-mem[knights[k].row][knights[k].col][i][j]+mem[pi][pj][i][j]+mem[knights[k].row][knights[k].col][pi][pj];
-                    min=Math.min(temp, min);
+                int min = Math.max(Math.abs(king.row - i), Math.abs(king.col - j));
+                for (int pi = kingStartRow; pi <= kingEndRow; pi++) {
+                    for (int pj = kingStartCol; pj <= kingEndCol; pj++) {
+                        for (int k = 1; k <= numKnights; k++) {
+                            int temp = Math.max(Math.abs(king.row - pi), Math.abs(king.col - pj))
+                                    - mem[knights[k].row][knights[k].col][i][j] + mem[pi][pj][i][j]
+                                    + mem[knights[k].row][knights[k].col][pi][pj];
+                            min = Math.min(temp, min);
+                        }
+                    }
                 }
-                minstep = Math.min(min+step, minstep);
+                minstep = Math.min(min + step, minstep);
             }
         }
 
@@ -98,15 +103,18 @@ public class camelot {
         System.out.println(System.currentTimeMillis() - startTime);
         System.exit(0);
     }
+
     static class Player {
         public int row, col;
-        public Player (int a, int b) {
+
+        public Player(int a, int b) {
             row = a;
             col = b;
         }
+
         public Player() {
-            row=0;
-            col=0;
+            row = 0;
+            col = 0;
         }
     }
 }
